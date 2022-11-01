fdde2de6ef97712df0ac0d6d896f6a694fb23afe
KhamzatKot <1000000000020z@gmail.com>
2022-11-01 19:51:37 +0300

    Это стартовый комит моей мини игры. В файл игры был добавлен первый обект с описанием основных характеристик персонажа и методами описывающими его возможности

---
+++ Soldier.js
@@ -0,0 +1,52 @@
+soldier = {
+    Name: 'Тигр',
+    health: 10 ,
+    weapon: {
+        namewe: '',
+        cartridges: 30,
+    },
+    supplies: 3,
+    shoot(){
+
+    },
+    hurt(){
+
+    },
+    reload(){
+        
+    },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
