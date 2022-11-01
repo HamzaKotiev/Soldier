da30ff9581d8a2f810ee88e04a1e928d0968e16a
KhamzatKot <1000000000020z@gmail.com>
2022-11-01 21:19:06 +0300

    Внес свои изменения в код

--- Soldier.js
+++ Soldier.js
@@ -18,22 +18,28 @@ soldier = {
     },
     hurt(){
         let text ;
-        if(this.supplies >= 1){
+        if(this.supplies > 1){
             this.weapon.cartridges = 30
             this.supplies -=1
             text = 'перезарядка...'
-        }else if(this.supplies <= 0){
+        }else if(this.supplies = 1){
+            this.weapon.cartridges = 30
+            this.supplies -=1
+            text = 'перезарядка...успешна, но припасов больше нет '
+        }else if(this.supplies <=0){
             text = 'Припасов НЕТ... '
         }
     },
     reload(){
         let text ;
-        if(this.health >1){
+        if(this.health > 1){
             this.health -= 1
-            text = 'Остарожне!! В гроб захотел '
-        }else if(this.health <= 1){
-            text = 'Ха пакойничек. ЛУЗЕР!!'
-
+            text = 'Ай в ноге больно!'
+        }else if(this.health = 1){
+            this.health -= 1
+            text = 'Осторожне!! В гроб захотел '
+        }else if(this.health <= 0){
+            text = 'Ха пакойничек. Валел хьйн хачи эйла!!'
         }
         
     },
