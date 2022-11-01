30329cb9a7a94bf854de47c4c8295f73d5b19e21
KhamzatKot <1000000000020z@gmail.com>
2022-11-01 20:43:54 +0300

    Ну что-же сделал функцыи теперь можно стрелять при этом тратяся патроны можно перезаряжать и тратить припасы и можно ранится или умереть

--- Soldier.js
+++ Soldier.js
@@ -7,12 +7,34 @@ soldier = {
     },
     supplies: 3,
     shoot(){
-
+        this.weapon.cartridges -=1;
+        let text ;
+        if(this.weapon.cartridges > 0){
+            text = 'бах-бах'
+        }else if(this.weapon.cartridges <= 0){
+            text = 'Перезарядись олух'
+        }
+        return text 
     },
     hurt(){
-
+        let text ;
+        if(this.supplies >= 1){
+            this.weapon.cartridges = 30
+            this.supplies -=1
+            text = 'перезарядка...'
+        }else if(this.supplies <= 0){
+            text = 'Припасов НЕТ... '
+        }
     },
     reload(){
+        let text ;
+        if(this.health >1){
+            this.health -= 1
+            text = 'Остарожне!! В гроб захотел '
+        }else if(this.health <= 1){
+            text = 'Ха пакойничек. ЛУЗЕР!!'
+
+        }
         
     },
 
